f76dc4802b2857913e779d6555252e6788614879
William Mucheru <willyk99@gmail.com>
2019-06-02 01:36:43 +0300

    Add LW calculation

--- eweigh/src/main/java/org/ilri/eweigh/MainActivity.java
+++ eweigh/src/main/java/org/ilri/eweigh/MainActivity.java
@@ -2,6 +2,12 @@ package org.ilri.eweigh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.ilri.eweigh.liveweight.LiveWeight;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText inputHG = findViewById(R.id.input_hg);
+        final TextView txtLW = findViewById(R.id.txt_lw);
+
+        inputHG.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if(count > 0){
+                    double heartGirth = Double.valueOf(s.toString());
+                    double liveWeight = LiveWeight.calculateLW(heartGirth);
+
+                    txtLW.setText(
+                            String.format("%s%s", getString(R.string.live_weight_label), liveWeight));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
 }
