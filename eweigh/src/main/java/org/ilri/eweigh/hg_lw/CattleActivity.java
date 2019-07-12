85f247011b9a4609506b774befcee039dba390b5
William Mucheru <willyk99@gmail.com>
2019-07-12 11:04:12 +0300

    Cattle, submissions list

--- eweigh/src/main/java/org/ilri/eweigh/hg_lw/CattleActivity.java
+++ eweigh/src/main/java/org/ilri/eweigh/hg_lw/CattleActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -13,13 +18,26 @@ import com.marcinorlowski.fonty.Fonty;
 import org.ilri.eweigh.R;
 import org.ilri.eweigh.accounts.AccountUtils;
 import org.ilri.eweigh.accounts.models.User;
+import org.ilri.eweigh.hg_lw.adapters.CattleAdapter;
+import org.ilri.eweigh.hg_lw.adapters.SubmissionsAdapter;
 import org.ilri.eweigh.hg_lw.models.Cattle;
 import org.ilri.eweigh.network.APIService;
 import org.ilri.eweigh.network.RequestParams;
 import org.ilri.eweigh.utils.URL;
+import org.json.JSONArray;
+import org.json.JSONException;
 
-public class CattleActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CattleActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
     public static final String TAG = CattleActivity.class.getSimpleName();
+
+    ListView listView;
+    List<Cattle> cattle = new ArrayList<>();
+
+    ProgressBar progressBar;
+    TextView blankState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +49,20 @@ public class CattleActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
+        progressBar = findViewById(R.id.progress_bar);
+        blankState = findViewById(R.id.txt_blank_state);
+
+        listView = findViewById(R.id.list_view_cattle);
+
         getCattle();
 
         Fonty.setFonts(this);
+    }
+
+    private void renderList(List<Cattle> cattle){
+        CattleAdapter adapter = new CattleAdapter(this, cattle);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
     }
 
     private void getCattle(){
@@ -47,12 +76,35 @@ public class CattleActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         Log.d(TAG, "Response: " + response);
+                        progressBar.setVisibility(View.GONE);
+
+                        try {
+                            JSONArray arr = new JSONArray(response);
+
+                            if(arr.length() > 0){
+                                blankState.setVisibility(View.GONE);
+
+                                for(int i=0; i<arr.length(); i++){
+                                    cattle.add(new Cattle(arr.getJSONObject(i)));
+                                }
+
+                                renderList(cattle);
+                            }
+                            else{
+                                blankState.setVisibility(View.VISIBLE);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
 
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Log.d(TAG, "Error: " + error.getMessage());
+                        progressBar.setVisibility(View.GONE);
+                        blankState.setVisibility(View.VISIBLE);
                     }
                 });
     }
@@ -68,5 +120,10 @@ public class CattleActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Cattle cattle = (Cattle) parent.getItemAtPosition(position);
     }
 }
---
+++ eweigh/src/main/java/org/ilri/eweigh/hg_lw/adapters/CattleAdapter.java
@@ -0,0 +1,62 @@
+package org.ilri.eweigh.hg_lw.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.marcinorlowski.fonty.Fonty;
+
+import org.ilri.eweigh.R;
+import org.ilri.eweigh.hg_lw.models.Cattle;
+
+import java.util.List;
+
+public class CattleAdapter  extends BaseAdapter {
+
+    private Context context;
+    private List<Cattle> cattle;
+    private LayoutInflater inflater;
+
+    public CattleAdapter(Context context, List<Cattle> cattle){
+        this.context = context;
+        this.cattle = cattle;
+    }
+
+    @Override
+    public int getCount() {
+        return cattle.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return cattle.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        if (inflater == null) {
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        if (view == null) {
+            view = inflater.inflate(R.layout.list_row_cattle, null);
+
+            Fonty.setFonts(parent);
+        }
+
+        Cattle c = (Cattle) getItem(position);
+
+        ((TextView) view.findViewById(R.id.txt_tag)).setText(c.getTag());
+        ((TextView) view.findViewById(R.id.txt_breed)).setText(String.format("Breed: %s", c.getBreed()));
+        ((TextView) view.findViewById(R.id.txt_date_created)).setText(String.format("Added On: %s", c.getCreatedOn()));
+
+        return view;
+    }
+}
