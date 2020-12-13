6ca1b4e76e19d4bbc822ec7114cb5175f18f8233
William Mucheru <willyk99@gmail.com>
2020-12-13 11:55:41 +0300

    Update MainActivity UI

--- app/src/main/java/org/ilri/eweigh/utils/URL.java
+++ app/src/main/java/org/ilri/eweigh/utils/URL.java
@@ -4,8 +4,8 @@ import org.ilri.eweigh.BuildConfig;
 
 public class URL {
 
-    private static String BASE_URL = BuildConfig.DEBUG ?
-            "http://192.168.1.104/Code/ilri/eweigh-api/" :
+    private static final String BASE_URL = BuildConfig.DEBUG ?
+            "http://192.168.1.102/eweigh-api/" :
             "https://app.eweighapp.com/";
 
     private static final String BASE_API_URL = BASE_URL + "api/v1/";
